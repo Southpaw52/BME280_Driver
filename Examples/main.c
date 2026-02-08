157e19bb3694f7f244ffece94d4b7c6e5a47d4d5
Southpaw52 <108016668+Southpaw52@users.noreply.github.com>
2026-02-08 16:49:27 +0300

    Update main.c

--- Examples/main.c
+++ Examples/main.c
@@ -100,4 +100,39 @@ static void MX_I2C1_Init(void)
     hi2c1.Instance = I2C1;
     hi2c1.Init.ClockSpeed = 100000;
     hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-    hi2c1.Ini
+    hi2c1.Init.OwnAddress1 = 0;
+    hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+    hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+    hi2c1.Init.OwnAddress2 = 0;
+    hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+    hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+
+    if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+/* GPIO Initialization Function */
+static void MX_GPIO_Init(void)
+{
+    __HAL_RCC_GPIOH_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+}
+
+/* Error Handler */
+void Error_Handler(void)
+{
+    __disable_irq();
+    while (1)
+    {
+        // Stay here if error occurs
+    }
+}
+
+#ifdef USE_FULL_ASSERT
+void assert_failed(uint8_t *file, uint32_t line)
+{
+    // Optional: report assert failure
+}
+#endif
